19159061e1077607ae154365905b04e1e6528a4a
Antonis <solwmos@gmail.com>
2020-08-31 20:33:14 +0300

    Adding Components and jquery

--- bgc-app/src/app/app.module.ts
+++ bgc-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainareaComponent } from './mainarea/mainarea.component';
+import { RightareaComponent } from './rightarea/rightarea.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MainareaComponent,
+    RightareaComponent
   ],
   imports: [
     BrowserModule,
