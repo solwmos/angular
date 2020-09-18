86ab99da1f1d45452587f8d7df6bb41a250881c9
solwmos <solwmos@gmail.com>
2020-09-18 23:59:25 +0300

    adding components at the homepage

--- shoppinglist-app/src/app/header/header.component.ts
+++ shoppinglist-app/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  recipeStatus="No recipe";
   constructor() { }
 
   ngOnInit(): void {
