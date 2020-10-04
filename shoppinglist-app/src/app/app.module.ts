93f8fdd0d9a458cb08fe4f847fe88b6e54146c47
Antonis <solwmos@gmail.com>
2020-10-04 18:32:15 +0300

    Adding jquery

--- shoppinglist-app/src/app/app.module.ts
+++ shoppinglist-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
@@ -16,6 +17,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     ShoppingListComponent,
     RecipeListComponent,
     RecipeDetailComponent,
+    RecipeItemComponent,
     HeaderComponent,
     RecipesComponent,
     ShoppingEditComponent
